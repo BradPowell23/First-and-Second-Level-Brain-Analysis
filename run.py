ae2d75068768239bb9acf73de1b6f6f3240ba33f
BradPowell23 <53798710+BradPowell23@users.noreply.github.com>
2022-12-04 18:37:27 -0800

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.insert(0, 'src')
+
 import nilearn
 import numpy as np
 import pandas as pd
@@ -11,67 +14,18 @@ from nilearn.plotting import plot_contrast_matrix
 import matplotlib.pyplot as plt
 from nilearn.glm import threshold_stats_img
 from numpy import array
+from nilearn.image import concat_imgs, index_img
 
-from src import *
-single_run("/Users/bradpowell/Desktop/sub-01/Functional/run_1.nii.gz")
-single_run("/Users/bradpowell/Desktop/sub-01/Functional/run_2.nii.gz")
-single_run("/Users/bradpowell/Desktop/sub-01/Functional/run_3.nii.gz")
-
-#!/usr/bin/env python
-
-import sys
-import os
-import json
-
-sys.path.insert(0, 'src')
-
-import env_setup
-from etl import get_data
-from features import apply_features
-
-from model import model_build
-
+from src import single_run
 
 def main(targets):
     '''
-    Runs the main project pipeline logic, given the targets.
-    targets must contain: 'data', 'analysis', 'model'. 
-    
-    `main` runs the targets in order of data=>analysis=>model.
+    Runs target test on the test data
     '''
-
-    env_setup.make_datadir()
-    env_setup.auth()
-    
-    
     if 'test' in targets:
-    
-
-    if 'data' in targets:
-        with open('config/data-params.json') as fh:
-            data_cfg = json.load(fh)
-
-        # make the data target
-        data = get_data(**data_cfg)
-
-    if 'features' in targets:
-        with open('config/features-params.json') as fh:
-            feats_cfg = json.load(fh)
-
-        feats, labels = apply_features(data, **feats_cfg)
-
-    if 'model' in targets:
-        with open('config/model-params.json') as fh:
-            model_cfg = json.load(fh)
-
-        # make the data target
-        model_build(feats, labels, **model_cfg)
-
+        single_run('test/testdata/test_1.nii.gz')
     return
 
-
 if __name__ == '__main__':
-    # run via:
-    # python main.py data features model
-    targets = sys.argv[1:]
+    targets = sys.argv[2:]
     main(targets)
