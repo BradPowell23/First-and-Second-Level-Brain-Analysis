c6e0f1b3e6c7ba1c4e362400aa49fa39d55e2b46
BradPowell23 <53798710+BradPowell23@users.noreply.github.com>
2022-12-05 14:41:29 -0800

    Update run.py

--- run.py
+++ run.py
@@ -2,21 +2,6 @@
 import sys
 sys.path.insert(0, 'src')
 
-import nilearn
-import numpy as np
-import pandas as pd
-from nilearn import plotting
-from nilearn import image
-from nilearn.glm.first_level import make_first_level_design_matrix
-from nilearn.plotting import plot_stat_map, plot_anat, plot_img, view_img
-from nilearn.glm.first_level import FirstLevelModel
-from nilearn.image import concat_imgs, mean_img
-from nilearn.plotting import plot_contrast_matrix
-import matplotlib.pyplot as plt
-from nilearn.glm import threshold_stats_img
-from numpy import array
-from nilearn.image import concat_imgs, index_img
-
 from src import single_run
 
 def main(targets):
